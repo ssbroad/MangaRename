096bf8529e8688fb65847613cbd6b33170f74f3a
Heng Xu <xuheng.pri@outlook.com>
2022-03-11 11:20:45 +0800

    Update

---
+++ MangaRename/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace MangaRename
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ShowInfo = new System.Windows.Forms.TextBox();
+            this.FolderPath = new System.Windows.Forms.TextBox();
+            this.Check = new System.Windows.Forms.Button();
+            this.Set = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ShowInfo
+            // 
+            this.ShowInfo.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ShowInfo.Location = new System.Drawing.Point(35, 152);
+            this.ShowInfo.Multiline = true;
+            this.ShowInfo.Name = "ShowInfo";
+            this.ShowInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ShowInfo.Size = new System.Drawing.Size(1158, 294);
+            this.ShowInfo.TabIndex = 0;
+            // 
+            // FolderPath
+            // 
+            this.FolderPath.Location = new System.Drawing.Point(35, 71);
+            this.FolderPath.Name = "FolderPath";
+            this.FolderPath.Size = new System.Drawing.Size(821, 21);
+            this.FolderPath.TabIndex = 1;
+            // 
+            // Check
+            // 
+            this.Check.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Check.Location = new System.Drawing.Point(894, 69);
+            this.Check.Name = "Check";
+            this.Check.Size = new System.Drawing.Size(95, 42);
+            this.Check.TabIndex = 2;
+            this.Check.Text = "Check";
+            this.Check.UseVisualStyleBackColor = true;
+            this.Check.Click += new System.EventHandler(this.Check_Click);
+            // 
+            // Set
+            // 
+            this.Set.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Set.Location = new System.Drawing.Point(1021, 69);
+            this.Set.Name = "Set";
+            this.Set.Size = new System.Drawing.Size(95, 42);
+            this.Set.TabIndex = 3;
+            this.Set.Text = "Set";
+            this.Set.UseVisualStyleBackColor = true;
+            this.Set.Click += new System.EventHandler(this.Set_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1215, 473);
+            this.Controls.Add(this.Set);
+            this.Controls.Add(this.Check);
+            this.Controls.Add(this.FolderPath);
+            this.Controls.Add(this.ShowInfo);
+            this.Name = "Form1";
+            this.Text = "MangaRename";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ShowInfo;
+        private System.Windows.Forms.TextBox FolderPath;
+        private System.Windows.Forms.Button Check;
+        private System.Windows.Forms.Button Set;
+    }
+}
+
---
+++ MangaRename/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MangaRename
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> new_Folder = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Check_Click(object sender, EventArgs e)
+        {
+            string path = FolderPath.Text;
+            DirectoryInfo root = new DirectoryInfo(path);
+            DirectoryInfo[] dics = root.GetDirectories();
+            FileInfo[] files = root.GetFiles();
+            List<string> showinfos = new List<string>();
+            List<string> group_artists = new List<string>();
+            List<string[]> artists = new List<string[]>();
+            foreach (var file in files)
+            {
+                bool find = false;
+                string fullpath = file.FullName;
+                string name = file.Name;
+                string group_artist = "test";
+                string group = "test";
+                string artist = "test";
+                //ShowInfo.Text += name + "\r\n";
+                if (Regex.Match(name, "(?<=\\[).*\\(.*\\)(?=])").Value != "")
+                {
+                    group_artist = Regex.Match(name, "(?<=\\[).*\\(.*\\)(?=])").Value;
+                    
+                    group = Regex.Match(group_artist, ".*(?=\\()").Value;
+                    artist = Regex.Match(group_artist, "(?<=\\().*(?=\\))").Value;
+                    //ShowInfo.Text += group_artist + "\r\n";
+                }
+                else if (Regex.Match(name, "(?<=\\[)[^\\[\\]]*(?=])").Value != "")
+                {
+                    artist = Regex.Match(name, "(?<=\\[)[^\\[\\]]*(?=])").Value;
+                    //artists.Add(new string[] { artist, name });
+                    //ShowInfo.Text += artist + "\r\n";
+                }
+                foreach (var dic in dics)
+                {
+                    string dic_name = dic.Name;
+                    //dic_name.Contains
+                    if (dic_name.Contains(group) || dic_name.Contains(artist))
+                    {
+                        find = true;
+                        //File.Move(fullpath, dic.FullName + "\\" + name);
+                        showinfos.Add(dic_name + " --> " + name + "\r\n");
+                        //ShowInfo.Text += dic_name + " --> " + name + "\r\n";
+                    }
+                    //else if (artist == dic_name)
+                    //{
+                    //    find = true;
+                    //    //File.Move(fullpath, dic.FullName + "\\" + name);
+                    //    ShowInfo.Text += dic_name + " --> " + name + "\r\n";
+                    //}
+                }
+                if (!find)
+                {
+                    if (artist.Contains("中国翻訳"))
+                    {
+                        showinfos.Add("发现匹配特例 - " + name + "\r\n");
+                        //ShowInfo.Text += "发现匹配特例 - " + name + "\r\n";
+                    }
+                    else
+                    {
+                        if (group_artist != "test")
+                        {
+                            group_artists.Add(group_artist);
+                            showinfos.Add("未找到对应目录 - " + group_artist + " / " + name + "\r\n");
+                        }
+                        else if(artist!= "test")
+                        {
+                            artists.Add(new string[] { artist, name });
+                            //showinfos.Add("未找到对应目录 - " + artist + " / " + name + "\r\n");
+                        }
+                        //showinfos.Add("未找到对应目录 - " + name + "\r\n");
+                        //ShowInfo.Text += "未找到对应目录 - " + name + "\r\n";
+                    }
+                }
+            }
+            foreach (var artist in artists)
+            {
+                bool find = false;
+                foreach (var group_artist in group_artists)
+                {
+                    if (group_artist.Contains(artist[0]))
+                    {
+                        showinfos.Add("未找到对应目录 - " + group_artist + " / " + artist[1] + "\r\n");
+                        find = true;
+                    }
+                }
+                if (!find)
+                {
+                    showinfos.Add("未找到对应目录 - " + artist[0] + " / " + artist[1] + "\r\n");
+                }
+            }
+            new_Folder = group_artists;
+            showinfos.Sort();
+            foreach (var showinfo in showinfos)
+            {
+                ShowInfo.Text += showinfo;
+            }
+        }
+
+        private void Set_Click(object sender, EventArgs e)
+        {
+            string path = FolderPath.Text;
+            foreach (var item in new_Folder)
+            {
+                Directory.CreateDirectory(path + "\\" + item);
+            }
+            DirectoryInfo root = new DirectoryInfo(path);
+            DirectoryInfo[] dics = root.GetDirectories();
+            FileInfo[] files = root.GetFiles();
+            foreach (var file in files)
+            {
+                bool find = false;
+                string fullpath = file.FullName;
+                string name = file.Name;
+                string group_artist = "test";
+                string group = "test";
+                string artist = "test";
+                if (Regex.Match(name, "(?<=\\[).*\\(.*\\)(?=])").Value != "")
+                {
+                    group_artist = Regex.Match(name, "(?<=\\[).*\\(.*\\)(?=])").Value;
+                    group = Regex.Match(group_artist, ".*(?=\\()").Value;
+                    artist = Regex.Match(group_artist, "(?<=\\().*(?=\\))").Value;
+                    //ShowInfo.Text += group_artist + "\r\n";
+                }
+                else if (Regex.Match(name, "(?<=\\[)[^\\[\\]]*(?=])").Value != "")
+                {
+                    artist = Regex.Match(name, "(?<=\\[)[^\\[\\]]*(?=])").Value;
+                    //ShowInfo.Text += artist + "\r\n";
+                }
+                foreach (var dic in dics)
+                {
+                    string dic_name = dic.Name;
+                    //dic_name.Contains
+                    if (dic_name.Contains(group) || dic_name.Contains(artist))
+                    {
+                        find = true;
+                        File.Move(fullpath, dic.FullName + "\\" + name);
+                        //showinfos.Add(dic_name + " --> " + name + "\r\n");
+                        //ShowInfo.Text += dic_name + " --> " + name + "\r\n";
+                    }
+                }
+                if (!find)
+                {
+                    Directory.CreateDirectory(path + "\\" + artist);
+                    File.Move(fullpath, path + "\\" + artist + "\\" + name);
+                    //if (group_artist!= "test")
+                    //{
+                    //    Directory.CreateDirectory(path+"\\"+ group_artist);
+                    //    File.Move(fullpath, path + "\\" + group_artist + "\\" + name);
+                    //}
+                    //else
+                    //{
+                    //    Directory.CreateDirectory(path + "\\" + artist);
+                    //    File.Move(fullpath, path + "\\" + artist + "\\" + name);
+                    //}
+                }
+            }
+        }
+    }
+}
